9f4eeda490042789de3dba8c157767da49227ca4
550651370@qq.com <huadong.chen@ldap.com>
2020-12-24 14:26:21 +0800

    redis

--- job-task/chd-order-job/src/main/java/task/JobApplication.java
+++ job-task/chd-order-job/src/main/java/task/JobApplication.java
@@ -1,11 +1,9 @@
 package task;
 
 import org.mybatis.spring.annotation.MapperScan;
-import org.springframework.amqp.rabbit.annotation.EnableRabbit;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.autoconfigure.domain.EntityScan;
-import org.springframework.cache.annotation.EnableCaching;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
 import org.springframework.scheduling.annotation.EnableScheduling;
