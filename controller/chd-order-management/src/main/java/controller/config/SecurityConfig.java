d0a1d5005fc1b5a85f648eb1081628425a2d73ab
550651370@qq.com <huadong.chen@ldap.com>
2020-12-24 16:00:51 +0800

    redis

---
+++ controller/chd-order-management/src/main/java/controller/config/SecurityConfig.java
@@ -0,0 +1,28 @@
+package controller.config;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+
+/**
+ * @author ：huadong.chen
+ * @ProjectName: controller$
+ * @Package: controller.config$
+ * @ClassName: SecurityConfig$
+ * @date ：Created in 12/24/20 3:51 PM
+ * @description：自定义权限认证配置
+ * @modified By：
+ * @version: 1.01
+ */
+@EnableWebSecurity
+public class SecurityConfig extends WebSecurityConfigurerAdapter {
+
+    @Override
+    protected void configure(HttpSecurity http) throws Exception {
+        http.authorizeRequests().antMatchers("/").permitAll()
+                .antMatchers("/user/**").hasRole("user")
+                .antMatchers("/user1/**").hasRole("user1");
+
+    }
+}
